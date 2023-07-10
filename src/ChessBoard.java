3320a9d82141d4dbf341307f07442e7487dab63e
Gleb <detroit167@mail.ru>
2023-07-11 00:08:39 +0300

    Added ChessBoard

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,11 +1,11 @@
 public class ChessBoard {
     public static void main(String[] args) {
-        String cb[][] = new String[8][8];
-        for (int i = 0; i < cb.length; i++) {
-            for (int j = 0; j < cb[i].length; j++) {
-                if ((i + j) % 2 == 0) cb[i][j] = "W";
-                else cb[i][j] = "B";
-                System.out.println(cb[i][j]);
+        String board[][] = new String[8][8];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if ((i + j) % 2 == 0) board[i][j] = "W";
+                else board[i][j] = "B";
+                System.out.println(board[i][j]);
 
             }
         }
