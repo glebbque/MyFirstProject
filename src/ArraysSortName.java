ebd0dd03a7e3a6b2028489f11eb5837dcef8669b
Gleb <detroit167@mail.ru>
2023-07-11 18:02:58 +0300

    Added sort name

---
+++ src/ArraysSortName.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ArraysSortName {
+    public static void main(String[] args) {
+        String name[] = {"Maxim", "Gleb", "Slava", "Olga", "Denis", "Yura" };
+        Arrays.sort(name);
+        System.out.println(Arrays.toString(name));
+
+
+    }
+}
