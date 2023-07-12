b4cc5a9baa5cc4188c31d85791bc4d9ce4c8c981
Gleb <detroit167@mail.ru>
2023-07-13 01:07:54 +0300

    Added arithmetic values

--- src/Lesson3/Sum2arrays.java
+++ src/Lesson3/Sum2arrays.java
@@ -4,29 +4,29 @@ import java.util.Arrays;
 
 public class Sum2arrays {
     public static void main(String[] args) {
-        int a[] = {1, 3, 5, 7, 9};
-        int b[] = {2, 4, 6, 8, 10};
+        int arr1[] = {1, 3, 5, 7, 9};
+        int arr2[] = {2, 4, 6, 8, 10};
         int sum = 0;
         int sum1 = 0;
-        for (int a1 : a) {
+        for (int a1 : arr1) {
             System.out.print(a1 + " ");
             sum += a1;
         }
         System.out.println();
-        for (int b1 : b) {
+        for (int b1 : arr2) {
             System.out.print(b1 + " ");
             sum1 += b1;
         }
-        int a2 = sum / a.length;
-        int b2 = sum1 / b.length;
+        int a2 = sum / arr1.length;
+        int b2 = sum1 / arr2.length;
         if (a2 > b2)
-            System.out.println("\nArrays arithmetic value " + Arrays.toString(a) + " > " + Arrays.toString(b));
+            System.out.println("\nArrays arithmetic value " + Arrays.toString(arr1) + " > " + Arrays.toString(arr2));
         else if (a2 < b2)
-            System.out.println("\nArrays arithmetic value " + Arrays.toString(a) + " < " + Arrays.toString(b));
+            System.out.println("\nArrays arithmetic value " + Arrays.toString(arr1) + " < " + Arrays.toString(arr2));
         else
             System.out.println("\nArrays arithmetic value equal");
 
-        System.out.println("\nArithmetic value:\n1 array - " + sum / a.length + "\n2 array - " + sum1 / b.length);
+        System.out.println("\nArithmetic value:\n1 array - " + sum / arr1.length + "\n2 array - " + sum1 / arr2.length);
 
 
     }
