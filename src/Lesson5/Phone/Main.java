b2226150bc963fd12adccd5f101704ddc71b3fda
Gleb <detroit167@mail.ru>
2023-07-26 20:14:16 +0300

    Added Phone

---
+++ src/Lesson5/Phone/Main.java
@@ -0,0 +1,22 @@
+package Lesson5.Phone;
+
+import static Lesson5.Phone.Phone.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Phone ph = new Phone();
+        Phone ph1 = new Phone(3211299, "Mi", 72f);
+        Phone ph2 = new Phone(9871234,"Samsung", 70f);
+        Phone ph3 = new Phone(4532378, "iPhone", 88f);
+
+        System.out.println(ph1);
+        System.out.println(ph2);
+        System.out.println(ph3);
+
+        ph.
+
+
+
+    }
+}
---
+++ src/Lesson5/Phone/Phone.java
@@ -0,0 +1,57 @@
+package Lesson5.Phone;
+
+
+public class Phone {
+    private int number;
+    private String model;
+    private float weight;
+
+    String name;
+
+    public Phone(int number, String model, float weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone(int number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone() {
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "number=" + number +
+                ", model='" + model + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
