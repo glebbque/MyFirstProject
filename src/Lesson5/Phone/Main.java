205a6150e3fcb8fb05b1102c37435fc4e78a0347
Gleb <detroit167@mail.ru>
2023-07-26 22:43:36 +0300

    Added Phone message

--- src/Lesson5/Phone/Main.java
+++ src/Lesson5/Phone/Main.java
@@ -5,18 +5,31 @@ import static Lesson5.Phone.Phone.*;
 public class Main {
     public static void main(String[] args) {
 
-        Phone ph = new Phone();
-        Phone ph1 = new Phone(3211299, "Mi", 72f);
-        Phone ph2 = new Phone(9871234,"Samsung", 70f);
-        Phone ph3 = new Phone(4532378, "iPhone", 88f);
+        Phone ph1 = new Phone("3211299", "Mi", 72f);
+        Phone ph2 = new Phone("9871234", "Samsung", 70f);
+        Phone ph3 = new Phone("4532378", "iPhone", 88f);
 
-        System.out.println(ph1);
-        System.out.println(ph2);
-        System.out.println(ph3);
+        System.out.println(ph1.getNumber());
+        System.out.println(ph1.getModel());
+        System.out.println(ph1.getWeight());
+        System.out.println();
 
-        ph.
+        System.out.println(ph2.getNumber());
+        System.out.println(ph2.getModel());
+        System.out.println(ph2.getWeight());
+        System.out.println();
 
+        System.out.println(ph3.getNumber());
+        System.out.println(ph3.getModel());
+        System.out.println(ph3.getWeight());
+        System.out.println();
 
+        ph1.receiveCall("Slava");
+        ph2.receiveCall("Katya");
+        ph3.receiveCall("Roman");
+        ph3.receiveCall("Petya", "3226441");
 
+        ph1.sendMessage("324324234", "234234234", "342342356");
+        ph2.sendMessage("234543654");
     }
 }
--- src/Lesson5/Phone/Phone.java
+++ src/Lesson5/Phone/Phone.java
@@ -1,20 +1,21 @@
 package Lesson5.Phone;
 
 
+import java.util.Arrays;
+
 public class Phone {
-    private int number;
+    private String number;
     private String model;
-    private float weight;
+    private double weight;
 
-    String name;
 
-    public Phone(int number, String model, float weight) {
+    public Phone(String number, String model, double weight) {
         this.number = number;
         this.model = model;
         this.weight = weight;
     }
 
-    public Phone(int number, String model) {
+    public Phone(String number, String model) {
         this.number = number;
         this.model = model;
     }
@@ -22,11 +23,11 @@ public class Phone {
     public Phone() {
     }
 
-    public int getNumber() {
+    public String getNumber() {
         return number;
     }
 
-    public void setNumber(int number) {
+    public void setNumber(String number) {
         this.number = number;
     }
 
@@ -38,20 +39,23 @@ public class Phone {
         this.model = model;
     }
 
-    public float getWeight() {
+    public double getWeight() {
         return weight;
     }
 
-    public void setWeight(float weight) {
+    public void setWeight(double weight) {
         this.weight = weight;
     }
 
-    @Override
-    public String toString() {
-        return "Phone{" +
-                "number=" + number +
-                ", model='" + model + '\'' +
-                ", weight=" + weight +
-                '}';
+    public void receiveCall(String name) {
+        System.out.println("Calling " + name);
+    }
+
+    public void receiveCall(String name, String number) {
+        System.out.println("Calling " + name + " with number " + number);
+    }
+
+    public void sendMessage(String... numbers) {
+        System.out.println(Arrays.toString(numbers));
     }
 }
